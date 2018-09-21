45ff868f495894d61e2753c135464878f8d1b6cb
Soroush M Pour <soroushmpour@gmail.com>
2018-09-21 20:14:24 +0430

    Update settings.py

--- djWeb/settings.py
+++ djWeb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dag(k**giu!3e_dh#b1=l+tfhgp9zelg=7+o72fy4pdcf+d)(h'
+SECRET_KEY = 'ENTER SOME SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
